e5925b8aa53dcfbedbc537fb6925f54fdaeb273a
Priyanka O <21082240+priya-gitTest@users.noreply.github.com>
2025-12-04 14:27:00 +0100

    Add climate_data table to demo schema
    Create a table for storing climate data with various attributes.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,12 @@
-
+CREATE TABLE IF NOT EXISTS "demo"."climate_data" (
+"timestamp" TIMESTAMP WITHOUT TIME ZONE,
+"geo_location" GEO_POINT,
+"data" OBJECT(DYNAMIC) AS (
+"temperature" DOUBLE PRECISION,
+"u10" DOUBLE PRECISION,
+"v10" DOUBLE PRECISION,
+"pressure" DOUBLE PRECISION,
+"latitude" DOUBLE PRECISION,
+"longitude" DOUBLE PRECISION
+)
+);
